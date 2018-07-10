a183df37a770d371d7faa6e95e2986f230e19513
nicholassmith897 <smith.komurov@gmail.com>
2018-07-10 09:39:43 -0500

    Stuff

--- ToDoList/src/app/app.component.ts
+++ ToDoList/src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  toDoInput: string = "";
+  todos = [];
+
+createToDo() {
+  const trimmedInput = this.toDoInput.trim();
+  console.log(this.toDoInput);
+  if (trimmedInput.length >= 1){
+    this.todos.push(this.toDoInput);
+    }
+  else {
+
+  }
+  this.toDoInput=""; 
+
+  //code that will create a todo
+}
+
+editToDo(todo) {
+  let index = this.todos.indexOf(todo);
+  console.log(index);
+  this.todos[index] = prompt("Please edit my existance", this.todos[index]);
+}
+
+deleteToDo(todo) {
+  let index = this.todos.indexOf(todo);
+  this.todos.splice(index, 1);
+  //deletes todo
+}
 }
